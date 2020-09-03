abad58e96f83ca4949cba584a7726f60b64fa5ff
jasmaa <jasonmaa3955@gmail.com>
2020-09-03 10:53:26 -0400

    Add twitter reminder

--- app.py
+++ app.py
@@ -4,12 +4,23 @@ from datetime import datetime
 import subprocess as sp
 from dotenv import load_dotenv
 import schedule
+import tweepy
 
 load_dotenv()
 
 RTMP_URL = os.getenv('RTMP_URL')
 STREAM_KEY = os.getenv('STREAM_KEY')
 INPUT_FILE = os.getenv('INPUT_FILE')
+
+TWITTER_API_KEY = os.getenv('TWITTER_API_KEY')
+TWITTER_API_SECRET = os.getenv('TWITTER_API_SECRET')
+TWITTER_ACCESS_TOKEN = os.getenv('TWITTER_ACCESS_TOKEN')
+TWITTER_ACCESS_TOKEN_SECRET = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
+STREAM_URL = os.getenv('STREAM_URL')
+
+auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET)
+auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
 
 command = [
     'ffmpeg',
@@ -37,7 +48,10 @@ def stream_siren():
     """Streams siren video to Twitch
     """
     # Check if first Wednesday of the month
-    if datetime.today().weekday() == 2 and datetime.today().day <= 7: 
+    today = datetime.today()
+    if today.weekday() == 2 and today.day <= 7:
+        date_str = today.strftime('%b %d, %Y')
+        api.update_status(f'Virtual siren for {date_str} has started at {STREAM_URL}!')
         print('Start stream...')
         sp.run(command)
 
