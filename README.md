2e1e56b0314ffa8b54820f6369f8c0520bfe7e9f
jasmaa <jasonmaa3955@gmail.com>
2023-07-03 14:04:37 -0400

    Switch reminder from Twitter to Mastodon

--- README.md
+++ README.md
@@ -1,45 +1,52 @@
 # Virtual Tornado Siren
 
-Google Cloud function to stream a video of a tornado siren to Twitch on the first Wednesday of every month at 11:55AM.
+Google Cloud function to stream a video of a tornado siren to Twitch on the
+first Wednesday of every month at 11:55AM.
 
 ## Setup
 
 ### Pre-requisites:
   - Twitch account
-  - Twitter account
+  - Mastodon account (optional)
   - GCP account
 
-### Setup Twitch and Twitter
+### Setup Twitch and Mastodon
   - Setup Twitch streaming
-    - Find your Twitch stream key (`STREAM KEY`) and appropriate [RTMP server](https://stream.twitch.tv/ingests/) (`RTMP_URL`)
+    - Find your Twitch stream key (`STREAM KEY`) and appropriate [RTMP
+      server](https://stream.twitch.tv/ingests/) (`RTMP_URL`)
     - Retrieve your Twitch account's URL. This will be `STREAM_URL`.
-  - Setup Twitter reminder
-    - Create a developer account on Twitter and retrieve your `TWITTER_API_KEY` and `TWITTER_API_SECRET`
-    - Generate access token with read and write permissions and retrieve the `TWITTER_ACCESS_TOKEN` and `TWITTER_ACCESS_TOKEN_SECRET`
+  - Setup Mastodon reminder (optional)
+    - Create a bot account on Mastodon instance of your choice. The url of the
+      instance will be `MSTDN_URL`.
+    - Go settings and create an application. This will generate an access token
+      which will be `MSTDN_ACCESS_TOKEN`.
 
 ### Deploying to GCloud
 
 Create a service user with `Cloud Function Invoker` privileges.
 
-Find an `mp4` video of a tornado siren of your choosing and host it as publically viewable on a static storage solution
-(Google Cloud Storage is one option). The path of the `mp4` will be `INPUT_FILE`.
+Find an `mp4` video of a tornado siren of your choosing and host it as
+publically viewable on a static storage solution (Google Cloud Storage is one
+option). The path of the `mp4` will be `INPUT_FILE`.
 
 Create a `.env.yaml` with and populate it with proper credentials.
 
 Deploy and schedule the function with:
 
-    # Deploy function
-    gcloud functions deploy stream_siren \
-    --runtime python37 \
-    --trigger-http \
-    --region <REGION> \
-    --env-vars-file .env.yaml \
-    --timeout 240
-    
-    # Create scheduled job
-    gcloud scheduler jobs create http stream-siren-job \
-    --schedule "55 11 * * *" \
-    --http-method GET \
-    --time-zone "America/New_York" \
-    --uri <TRIGGER URI> \
-    --oidc-service-account-email <SERVICE ACCOUNT EMAIL>
+```bash
+# Deploy function
+gcloud functions deploy stream_siren \
+  --runtime python37 \
+  --trigger-http \
+  --region <REGION> \
+  --env-vars-file .env.yaml \
+  --timeout 240
+
+# Create scheduled job
+gcloud scheduler jobs create http stream-siren-job \
+  --schedule "55 11 * * *" \
+  --http-method GET \
+  --time-zone "America/New_York" \
+  --uri <TRIGGER URI> \
+  --oidc-service-account-email <SERVICE ACCOUNT EMAIL>
+```
--- main.py
+++ main.py
@@ -1,54 +1,56 @@
 import os
+import urllib.parse
 import sys
-import time
 from datetime import datetime
 import subprocess as sp
 import logging
-import tweepy
+import requests
 import pytz
 
-# Set timezone
-try:
-    tz = pytz.timezone(os.getenv('TZ'))
-except pytz.exceptions.UnknownTimeZoneError:
-    tz = pytz.timezone('UTC')
-
-RTMP_URL = os.getenv('RTMP_URL')
-STREAM_KEY = os.getenv('STREAM_KEY')
-INPUT_FILE = os.getenv('INPUT_FILE')
-
-TWITTER_API_KEY = os.getenv('TWITTER_API_KEY')
-TWITTER_API_SECRET = os.getenv('TWITTER_API_SECRET')
-TWITTER_ACCESS_TOKEN = os.getenv('TWITTER_ACCESS_TOKEN')
-TWITTER_ACCESS_TOKEN_SECRET = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
-STREAM_URL = os.getenv('STREAM_URL')
-
-# Setup tweepy
-auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET)
-auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth)
-
-# Setup logger
-log = logging.getLogger(__name__)
-out_hdlr = logging.StreamHandler(sys.stdout)
-out_hdlr.setFormatter(logging.Formatter('%(asctime)s: %(message)s'))
-out_hdlr.setLevel(logging.INFO)
-log.addHandler(out_hdlr)
-log.setLevel(logging.INFO)
 
 def stream_siren(request):
     """Streams siren video to Twitch
     """
+    RTMP_URL = os.getenv('RTMP_URL')
+    STREAM_KEY = os.getenv('STREAM_KEY')
+    INPUT_FILE = os.getenv('INPUT_FILE')
+
+    MSTDN_ACCESS_TOKEN = os.getenv('MSTDN_ACCESS_TOKEN')
+    MSTDN_URL = os.getenv('MSTDN_URL')
+    STREAM_URL = os.getenv('STREAM_URL')
+
+    # Setup logger
+    log = logging.getLogger(__name__)
+    out_hdlr = logging.StreamHandler(sys.stdout)
+    out_hdlr.setFormatter(logging.Formatter('%(asctime)s: %(message)s'))
+    out_hdlr.setLevel(logging.INFO)
+    log.addHandler(out_hdlr)
+    log.setLevel(logging.INFO)
+
+    # Set timezone
+    try:
+        tz = pytz.timezone(os.getenv('TZ'))
+    except pytz.exceptions.UnknownTimeZoneError:
+        tz = pytz.timezone('UTC')
+
     # Check if first Wednesday of the month
     today = datetime.today().astimezone(tz)
     if today.weekday() == 2 and today.day <= 7:
-        # Tweet reminder
-        log.info('Tweeting reminder...')
+        # Toot reminder
+        log.info('Tooting reminder...')
         try:
             date_str = today.strftime('%b %d, %Y')
-            api.update_status(f'Virtual siren for {date_str} has started at {STREAM_URL}!')
-        except tweepy.TweepError:
-            log.warning('Could not send tweet.')
+            msg = f'Virtual siren for {date_str} has started at {STREAM_URL}!'
+            requests.post(
+                urllib.parse.urljoin(MSTDN_URL, "/api/v1/statuses"),
+                headers={
+                    "Authorization": f"Bearer {MSTDN_ACCESS_TOKEN}",
+                }, data={
+                    "status": msg,
+                },
+            )
+        except Exception:
+            log.warning('Could not send toot.')
 
         # FFMPEG stream to RTMP server
         log.info('Starting stream...')
